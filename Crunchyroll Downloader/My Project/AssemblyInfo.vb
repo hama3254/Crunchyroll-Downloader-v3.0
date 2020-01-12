2cc7585bd31175090cda302a898c404314d4304c
hama3254 <hama32546@web.de>
2020-01-12 16:29:16 +0100

    updated version number

--- Crunchyroll Downloader/My Project/AssemblyInfo.vb
+++ Crunchyroll Downloader/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("3.0.1")>
-<Assembly: AssemblyFileVersion("3.0.5")>
+<Assembly: AssemblyVersion("3.1.0")>
+<Assembly: AssemblyFileVersion("3.1.0")>
 <Assembly: NeutralResourcesLanguage("")>
